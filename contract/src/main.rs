d9009f5dc60328e08df9279949f82356aae5e113
Karan Dhareshwar <karan@casperlabs.io>
2020-11-05 11:01:18 -0600

    Update hello_world to use macros

--- contract/src/main.rs
+++ contract/src/main.rs
@@ -1,32 +1,52 @@
-#![cfg_attr(
-    not(target_arch = "wasm32"),
-    crate_type = "target arch should be wasm32"
-)]
 #![no_main]
 
-use casperlabs_contract::{
+extern crate alloc;
+use alloc::{collections::BTreeSet, string::String};
+use std::convert::TryInto;
+
+use casperlabs_contract_macro::{casperlabs_constructor, casperlabs_contract, casperlabs_method};
+
+use contract::{
     contract_api::{runtime, storage},
+    unwrap_or_revert::UnwrapOrRevert,
 };
-use casperlabs_types::{Key, URef};
 
+use types::{
+    bytesrepr::ToBytes,
+    contracts::{EntryPoint, EntryPointAccess, EntryPointType, EntryPoints},
+    runtime_args, CLType, CLTyped, CLValue, Group, Parameter, RuntimeArgs, URef,
+};
+/// The name of the key under which the string will be stored
 const KEY: &str = "special_value";
-const ARG_MESSAGE: &str = "message";
+/// The value of the string to be stored
+const ARG_MESSAGE: &str = "hello world";
 
-fn store(value: String) {
-    // Store `value` under a new unforgeable reference.
-    let value_ref: URef = storage::new_uref(value);
 
-    // Wrap the unforgeable reference in a value of type `Key`.
-    let value_key: Key = value_ref.into();
+#[casperlabs_contract]
+mod hello_world {
 
-    // Store this key under the name "special_value" in context-local storage.
-    runtime::put_key(KEY, value_key);
+    #[casperlabs_constructor]
+    fn init() {
+        let value = String::from(ARG_MESSAGE);
+        set_key(KEY, value);
+    }
+
+    #[casperlabs_method]
+    fn update(value: String) {
+        set_key(KEY, value)
+    }
+
+    fn set_key<T: ToBytes + CLTyped>(name: &str, value: T) {
+        match runtime::get_key(name) {
+            Some(key) => {
+                let key_ref = key.try_into().unwrap_or_revert();
+                storage::write(key_ref, value);
+            }
+            None => {
+                let key = storage::new_uref(value).into();
+                runtime::put_key(name, key)
+            }
+        }
+    }
 }
 
-// All session code must have a `call` entrypoint.
-#[no_mangle]
-pub extern "C" fn call() {
-    // Get the optional first argument supplied to the argument.
-    let value: String = runtime::get_named_arg(ARG_MESSAGE);
-    store(value);
-}
---
+++ tests/src/hello_world.rs
@@ -0,0 +1,67 @@
+use casperlabs_engine_test_support::{Code, Hash, SessionBuilder, TestContext, TestContextBuilder};
+use casperlabs_types::{
+    account::AccountHash, bytesrepr::FromBytes, runtime_args, CLTyped, RuntimeArgs, U512,
+};
+
+pub const TEST_ACCOUNT: AccountHash = AccountHash::new([7u8; 32]);
+pub const HELLO_WORLD: &str = "hello_world";
+pub const HELLO_WORLD_HASH: &str = "hello_world_hash";
+
+pub struct HelloWorldContract {
+    pub context: TestContext,
+    pub hello_world_hash: Hash,
+}
+
+impl HelloWorldContract {
+    pub fn deploy() -> Self {
+        let mut context = TestContextBuilder::new()
+            .with_account(TEST_ACCOUNT, U512::from(128_000_000))
+            .build();
+        let session_code = Code::from("contract.wasm");
+        let session = SessionBuilder::new(session_code, runtime_args!{})
+            .with_address(TEST_ACCOUNT)
+                .with_authorization_keys(&[TEST_ACCOUNT])
+                .build();
+        context.run(session);
+        let hello_world_hash = Self::contract_hash(&context, HELLO_WORLD_HASH);
+        Self {
+            context,
+            hello_world_hash
+        }
+    }
+
+    pub fn contract_hash(context: &TestContext, name: &str) -> Hash {
+        context
+            .query(TEST_ACCOUNT, &[name])
+            .unwrap_or_else(|_| panic!("{} contract not found", name))
+            .into_t()
+            .unwrap_or_else(|_| panic!("{} is not type Contract", name))
+    }
+
+    pub fn call_update(&mut self, value: String) {
+        let code = Code::Hash(self.hello_world_hash, "update".to_string());
+        let args = runtime_args! {
+            "value" => value
+        };
+        let session = SessionBuilder::new(code, args)
+            .with_address(TEST_ACCOUNT)
+            .with_authorization_keys(&[TEST_ACCOUNT])
+            .build();
+        self.context.run(session);
+    }
+    pub fn query_contract<T: CLTyped + FromBytes>(&self, name: &str) -> Option<T> {
+        match self
+            .context
+            .query(TEST_ACCOUNT, &[HELLO_WORLD, &name.to_string()])
+        {
+            Err(_) => None,
+            Ok(maybe_value) => {
+                let value = maybe_value
+                    .into_t()
+                    .unwrap_or_else(|_| panic!("{} is not expected type.", name));
+                Some(value)
+            }
+        }
+    }
+
+}
