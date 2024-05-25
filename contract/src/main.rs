0e851806d72c86f55ff368b3e97d12b543232b9d
gRoussac <gRoussac@users.noreply.github.com>
2024-05-25 22:00:38 +0200

    Proposition of change to add custome key name

--- contract/src/main.rs
+++ contract/src/main.rs
@@ -7,8 +7,8 @@ use alloc::string::String;
 use casper_contract::contract_api::{runtime, storage};
 use casper_types::{Key, URef};
 
-const KEY_NAME: &str = "my-key-name";
-const RUNTIME_ARG_NAME: &str = "message";
+const RUNTIME_ARG_KEY_NAME: &str = "key-name";
+const RUNTIME_ARG_MESSAGE: &str = "message";
 
 fn store(value: String, key_name: String) {
     // Store `value` under a new unforgeable reference.
@@ -17,18 +17,20 @@ fn store(value: String, key_name: String) {
     // Wrap the unforgeable reference in a value of type `Key`.
     let value_key: Key = value_ref.into();
 
+    // Remove the key if already exists in account entity context
     runtime::remove_key(&key_name);
 
-    // Store this key under the name "my-key-name" in caller context
+    // Store this key under the name "key-name" in caller context
     runtime::put_key(&key_name, value_key);
 }
 
 // All session code must have a `call` entrypoint.
 #[no_mangle]
 pub extern "C" fn call() {
-    // This contract expects a single runtime argument to be provided.  The arg is named "message"
-    // and will be of type `String`.
-    let value: String = runtime::get_named_arg(RUNTIME_ARG_NAME);
-    let key_name: String = runtime::get_named_arg(KEY_NAME);
-    store(value, key_name);
+    // This contract expectstwo runtime arguments to be provided.
+    // The arg is named "message"and will be of type `String`.
+    let message: String = runtime::get_named_arg(RUNTIME_ARG_MESSAGE);
+    // The arg is named "key-name" and will be of type `String`.
+    let key_name: String = runtime::get_named_arg(RUNTIME_ARG_KEY_NAME);
+    store(message, key_name);
 }
